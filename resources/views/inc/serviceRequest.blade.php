b9b2ffdaab92d72e50f40da85714659e03d6b041
naufalmaulana <naufallmaulana@yahoo.com>
2022-01-05 15:16:49 +0700

    [Frontend] Service request modal update

--- resources/views/inc/serviceRequest.blade.php
+++ resources/views/inc/serviceRequest.blade.php
@@ -7,18 +7,23 @@
         </div>
         <div class="modal-header d-block text-center">
             <img class="mb-3" src="/assets/circle-check.svg" alt="">
-            <h5 class="modal-title fw-bold" id="staticBackdropLabel">Maintenance Requested</h5>
-          {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
+            <h5 class="modal-title fw-bold" id="staticBackdropLabel">Request Maintenance</h5>
+          <!-- {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}} -->
         </div>
-        <div class="modal-body text-center">
-            Your maintenance has been requested!
-        </div>
-        <div class="modal-footer justify-content-center">
-          <form id="serviceRequestForm" method="POST">
-            @csrf
-            <button type="submit" class="btn bg-blue text-white fw-bold px-5">Agree</button>
-           </form>
-        </div>
+        <form id="serviceRequestForm" action="POST">
+          <div class="modal-body ">
+            <label for="company" class="form-label">Select Company</label>
+            <div class=" mb-3 rounded ">
+              <select id="company" name="company" class="form-select">
+                <option value="mro1">MRO 1</option>
+                <option value="mro2">MRO 2</option>
+              </select>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+              <button type="submit" class="btn bg-blue text-white fw-bold px-5">Agree</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
